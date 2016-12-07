f2232999bb963a03fb9b86e838aaf5bcdd67594f
brendandoherty <brendan@storganize.io>
2016-12-06 20:29:22 -0500

    Working with the google maps API

--- BrendansRepo/MoverApp/codestructure/js/index.js
+++ BrendansRepo/MoverApp/codestructure/js/index.js
@@ -2,9 +2,9 @@
 
 var model = {
 	email: undefined,
-	startAddress: "3212 Morrison St",
-	endAddress: "3378 Stephenson Pl",
-	distance: "2 miles"
+	origin_addresses: '',
+	destination_addresses: '',
+  distance: ''
 };
 
 // View
@@ -28,6 +28,7 @@ function renderForm() {
 function renderSuccess() {
   var successHtml = successTemplate(model);
   $('#successContainer').html(successHtml);
+  document.getElementById("formContainer").style.display="none";
 };
 
 // Controller
@@ -39,7 +40,27 @@ function setup() {
   // Event Listeners
 
 $('#formContainer').on('click', '#submitbutton', handleSubmit)
+
+
+$('#gridCheckbox3').on('click', function() {
+   if($(this).is(':checked')) {
+      document.getElementById("currentStairs").style.display="block";
+   } else {
+      document.getElementById("currentStairs").style.display="none";
+   }
+});
+
+$('#gridCheckbox4').on('click', function() {
+   if($(this).is(':checked')) {
+      document.getElementById("newStairs").style.display="block";
+   } else {
+      document.getElementById("newStairs").style.display="none";
+   }
+});
 };
+
+
+// Form Controllers
 
 
 function handleSubmit() {
@@ -49,7 +70,38 @@ function handleSubmit() {
     email: email,
   });
 
+  // Google Maps API Reference
+var addressLine1 = $('input[id="dest-address-line1"]').val();
+
+var originStreetNum = $('input[id="originStreetNum"]').val();
+var originStreetName = $('input[id="originStreetName"]').val();
+var originStreetType = $('input[id="originStreetType"]').val();
+var originCity = $('input[id="originCity"]').val();
+var originState = $('input[id="originState"]').val();
+
+var destinationStreetNum = $('input[id="destinationStreetNum"]').val();
+var destinationStreetName = $('input[id="destinationStreetName"]').val();
+var destinationStreetType = $('input[id="destinationStreetType"]').val();
+var destinationCity = $('input[id="destinationCity"]').val();
+var destinationState = $('input[id="destinationState"]').val();
+
+var addressTest = addressLine1.split(" ")
+
+var newAddress = addressTest[0] + '+' + addressTest[1] + '+' + addressTest[2] + '+' + addressTest[3]
+
+var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=';
+url += newAddress + '+' + originCity + '+' + originState + '&destinations=';
+url += destinationStreetNum + '+' + destinationStreetName + '+' + destinationStreetType + '+' + destinationCity + '+' + destinationState;
+url += '&key=AIzaSyCgiwtC_K-CKQ5qGkUBHCyuL4LdsrPAkDQ'
+console.log(url)
+
+$.get(url, function(data) {
+  model.origin_addresses = data.origin_addresses;
+  model.destination_addresses = data.destination_addresses;
+  model.distance = data.rows.distance.text;
+})
+
   renderSuccess();
-}
+};
 
 $(document).ready(setup);
